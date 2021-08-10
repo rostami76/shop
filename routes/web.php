cf9ecd5a99ebf538d756d9ce4b652b6fd0210b04
rostami76 <amirhossein.00181818@gmail.com>
2021-08-10 11:37:11 +0430

    admin panel theme

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('client.index');
 });
+
+Route::get('/panel', function () {
+    return view('admin.index');
+});
+
+
